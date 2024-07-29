4bbfc0d8d45f94a8df3efee0ba0dae1211821eaa
Oyama <oyamasscruz@gmail.com>
2024-07-29 08:25:20 -0300

    creating section of attractions and creating java code for the tabs. All styling for this sections it's finish too.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,31 @@
-AOS.init();
+document.addEventListener('DOMContentLoaded', function() {
+    const buttonTab = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttonTab.length; i++) {
+        buttonTab[i].addEventListener('click', function(btn) {
+            const tabTarget = btn.target.dataset.tabButton;
+            const tab = document.querySelector(`[data-tab-id="${tabTarget}"]`);
+            hideContain();
+            tab.classList.add('attractions__tab__content--is-active');
+            hideStyleButton();
+            btn.target.classList.add('attractions__tab__button--is-active');
+        })
+    }
+})
+
+function hideContain() {
+    const tabContain = document.querySelectorAll('[data-tab-id]');
+    
+    for (let i = 0; i < tabContain.length; i++) {
+        tabContain[i].classList.remove('attractions__tab__content--is-active');
+
+    }
+}
+
+function hideStyleButton() {
+    const buttonTab = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttonTab.length; i++) {
+        buttonTab[i].classList.remove('attractions__tab__button--is-active');
+    }
+}
