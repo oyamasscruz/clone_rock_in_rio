aeab4237fa92001be2410b8551f8d05be025cc1b
Oyama <oyamasscruz@gmail.com>
2024-07-30 08:40:13 -0300

    ending the devices compatibles

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
+AOS.init();
+
+const eventDate = new Date("Sep 13, 2024 20:00:00");
+const stampEventDate = eventDate.getTime();
+
+const countingTimeRemaining = setInterval(function() {
+    
+    const now = new Date();
+    const stampNow = now.getTime();
+    const timeWeAreLookingFor = stampEventDate - stampNow;
+
+    const dayInMilliseconds = (1000 * 60 * 60 * 24);
+    const hoursInMilliseconds = (1000 * 60 * 60);
+    const minutesInMilliseconds = (1000 * 60);
+    const secondsInMilliseconds = (1000);
+
+    const dayWeAreLookingFor = Math.floor(timeWeAreLookingFor / dayInMilliseconds)
+    const hourWeAreLookingFor = Math.floor(((timeWeAreLookingFor % dayInMilliseconds) / hoursInMilliseconds));
+    const minutesWeAreLookingFor = Math.floor(((timeWeAreLookingFor % hoursInMilliseconds) / minutesInMilliseconds));
+    const secondsWeAreLookingFor = Math.floor(((timeWeAreLookingFor % minutesInMilliseconds) / secondsInMilliseconds));
+
+    document.getElementById('timer').innerHTML = ` ${dayWeAreLookingFor}d ${hourWeAreLookingFor}h ${minutesWeAreLookingFor}m ${secondsWeAreLookingFor}s`;
+
+    if (timeWeAreLookingFor < 0) {
+        clearInterval(countingTimeRemaining);
+        document.getElementById('timer').innerHTML = 'Evento Expirado';
+    }
+
+},1000)
+
 document.addEventListener('DOMContentLoaded', function() {
     const buttonTab = document.querySelectorAll('[data-tab-button]');
 
@@ -29,3 +59,4 @@ function hideStyleButton() {
         buttonTab[i].classList.remove('attractions__tab__button--is-active');
     }
 }
+
